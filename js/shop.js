7dee3b4bd0d7fcb1c09791fe30abab75dc6a780b
Sage [It/She/They] <95890273+SpicedSage@users.noreply.github.com>
2022-10-07 16:17:14 -0400

    started on shop
    day 3 started drafting ideas for shop and experimenting with implementation, last save 1612

---
+++ js/shop.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+
+	let elements = document.getElementsByClassName("ecsp-title");
+	for (let i = 0; i < elements.length; i++) {
+		elements[i].style.color = 'white';
+	}
+};
